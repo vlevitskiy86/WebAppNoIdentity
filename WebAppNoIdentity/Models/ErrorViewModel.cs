57f391083543acf43bb38c11e19a915f0ff6bc6d
Volodymyr Levytskyi <levitskiy86@gmail.com>
2018-07-27 11:33:17 +0300

    First commit

---
+++ WebAppNoIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppNoIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
